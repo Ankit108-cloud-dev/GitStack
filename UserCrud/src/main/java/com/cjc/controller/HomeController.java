51d03dbc6ae56ef532bbded7ec464992995110c1
rishi942 <rishabhridhorkar@gmail.com>
2024-07-04 16:26:23 +0530

    Added User

--- UserCrud/src/main/java/com/cjc/controller/HomeController.java
+++ UserCrud/src/main/java/com/cjc/controller/HomeController.java
@@ -1,5 +1,21 @@
 package com.cjc.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cjc.modle.User;
+
+@RestController
 public class HomeController {
+	
+	@PostMapping
+	public String postData(@RequestBody User user) {
+		
+		
+		
+		return "Data Added";
+	}
+
 
 }
---
+++ UserCrud/src/main/java/com/cjc/modle/User.java
@@ -0,0 +1,5 @@
+package com.cjc.modle;
+
+public class User {
+
+}
