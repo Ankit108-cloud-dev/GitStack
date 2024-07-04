2b6330e3ba873b36e3c8255be29a3afa6a7f3075
Ankit108-cloud-dev <ank108108108@gmail.com>
2024-07-04 16:23:40 +0530

    added User

--- UserCrud/src/main/java/com/cjc/controller/HomeController.java
+++ UserCrud/src/main/java/com/cjc/controller/HomeController.java
@@ -1,5 +1,21 @@
 package com.cjc.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+	
+	
+	@GetMapping
+	public List<String> getData(){
+		
+		
+		
+		return null;
+	}
+	
 
 }
---
+++ UserCrud/src/main/java/com/cjc/modle/User.java
@@ -0,0 +1,11 @@
+package com.cjc.modle;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	int uid;
+	String name;
+
+}
